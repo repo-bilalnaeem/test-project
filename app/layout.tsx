6d809130451cd20c6f9f2d0a3bddca6355a8cdd6
Bilal Naeem <94391016+repo-bilalnaeem@users.noreply.github.com>
2025-04-04 12:42:11 +0500

    removing unused variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,6 @@ import { Geist, Geist_Mono, Montserrat } from "next/font/google";
 
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
